c459858c96b985c7b29fef5e0f2b72cf889dbbf5
Alicja <alichr07@gmail.com>
2022-03-19 18:46:16 +0100

    Dodaj pliki projektów.

---
+++ Palindrom/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace Palindrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj słowo/zdanie: ");
+            string text = new string(Console.ReadLine());
+
+            CheckPalindrom(text);
+
+            static void CheckPalindrom(string text)
+            {
+                text = text.Replace(" ", "").ToLower();
+
+                string zdanie = new string(text.ToCharArray().Reverse().ToArray());
+
+                if (zdanie == text && !String.IsNullOrEmpty(text))
+                {
+                    Console.WriteLine("To jest palindrom.");
+                }
+                else if (String.IsNullOrEmpty(text))
+                {
+                    Console.WriteLine("Nie wpisano żadnego słowa.");
+                }
+                else
+                {
+                    Console.WriteLine("To nie jest palindrom.");
+                }
+            }
+        }
+    }
+}
